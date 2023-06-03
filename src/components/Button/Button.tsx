7bf3e63948ad844c7bfd6677615d921046b26831
Alberto Rodriguez <ticoc911@gmail.com>
2023-06-03 22:29:05 +0200

    button componet done

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -12,12 +12,14 @@ export const Button: FC<Props> = ({text}) => {
 
     const onClick = () => {
         console.log('Redirect to email')
+        window.location.href='mailto: ticoc911@gmall.com'
     }
 
     return (
         <button 
             onClick={onClick}
-            className={`${space.className} text-primary`}
+            className={`${space.className} text-primary border-2 border-primary border-solid py-3 px-6 rounded-full
+            hover:bg-primary-2`}
         >
             {text}
         </button>
