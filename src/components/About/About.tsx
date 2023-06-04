16bea9dfd118be0488947791b9aaf86edadaf801
Alberto Rodriguez <ticoc911@gmail.com>
2023-06-05 00:32:30 +0200

    center avatar

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -35,7 +35,7 @@ export const About = () => {
           </ul>
         </div>
 
-        <div className="relative max-w-[300px] mt-12 mr-auto mb-0 w-[70%] md:m-0 md:max-w-[300px]">
+        <div className="relative max-w-[300px] mt-12 mx-auto mb-0 w-[70%] md:m-0 md:max-w-[300px] sm:mr-auto">
           <div className="block relative w-full rounded-xl before:block before:absolute before:w-full before:h-full before:rounded-xl
             before:top-0  before:left-0 before:bg-white before:[mix-blend-mode:screen] after:block 
             after:absolute after:w-full after:h-full after:rounded-xl after:top-5  after:left-5 after:border-2 
--- src/components/NumberedHeading/NumberedHeading.tsx
+++ src/components/NumberedHeading/NumberedHeading.tsx
@@ -8,7 +8,7 @@ export const NumberedHeading: FC<Props> = ({title}) => {
     return (
         <h2 className='flex items-center relative mt-2 mr-0 mb-10 w-full font-medium text-dynamicH2 text-white whitespace-nowrap
         before:relative before:bottom-0 before:[counter-increment:section] before:content-["0"_counter(section)_"."]  
-        before:text-primary before:font-space before:text-dynamicSection before:mr-1 before:mb-[-4px] md:before:mr-4
+        before:text-primary before:font-space before:text-dynamicSection before:mr-1 before:mb-[-5px] md:before:mr-4
         after:block after:relative after:top-0 after:w-full after:h-[1px] after:ml-3 after:bg-primary sm:after:ml-5 
         sm:after:w-[200px] md:after:w-[300px]'
         >
