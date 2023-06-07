f4c3e67367b3882da164a6160167c089fe7af48b
Alberto Rodriguez <ticoc911@gmail.com>
2023-06-07 16:28:51 +0200

    styled featured section

--- src/components/Featured/Featured.tsx
+++ src/components/Featured/Featured.tsx
@@ -8,20 +8,21 @@ import Image from "next/image";
 export const Featured = () => {
   return (
     <section>
-      <NumberedHeading title="Featured Projects"/>
+      <NumberedHeading title="Projects" />
 
-      <div className="flex flex-col gap-13 sm:gap-24">
+      <div className="flex flex-col gap-16 sm:gap-24">
         {
           featured.map((project, idx) => {
             const { title, external, techs, github, cover, descriptionHtml } = project;
 
             return (
-              <div key={title} className="grid gap-[10px] grid-cols-12 items-center group">
-                <div className="relative col-[1_/_7] row-span-full group-odd:text-right group-odd:col-[7/-1]">
+              <div key={title} className="grid gap-[10px] grid-cols-12 items-center group relative">
+                <div className="relative col-[1_/_-1] row-span-full group-odd:text-right group-odd:col-[1/-1]
+                  sm:col-[1_/_7] sm:group-odd:col-[7/-1]">
                   <p className="my-[10px] text-primary font-space text-xs">
                     Featured Project
                   </p>
-                  <h3 className="text-white text-[clamp(24px,5vw,28px)] leading-10">
+                  <h3 className="text-white text-[clamp(21px,5vw,28px)] leading-10">
                     {title}
                   </h3>
                   <div
@@ -42,14 +43,14 @@ export const Featured = () => {
 
                   <div className="flex items-center relative mt-[10px] ml-[-10px] group-odd:justify-end">
                     {github && (
-                      <a 
+                      <a
                         className="p-[10px]"
-                        rel="noreferrer" 
-                        target="_blank" 
-                        href={github} 
+                        rel="noreferrer"
+                        target="_blank"
+                        href={github}
                         aria-label="GitHub Link"
                       >
-                        <AiOutlineGithub 
+                        <AiOutlineGithub
                           className="w-5 h-5 hover:text-white transition-colors "
                         />
                       </a>
@@ -62,7 +63,7 @@ export const Featured = () => {
                         href={external}
                         aria-label="External Link"
                       >
-                        <FiExternalLink 
+                        <FiExternalLink
                           className="w-5 h-5 hover:text-white transition-colors"
                         />
                       </a>
@@ -70,16 +71,18 @@ export const Featured = () => {
                   </div>
                 </div>
 
-                <div className="shadow-sm col-[6_/_-1] row-span-full relative z-10 w-full max-w-full group-odd:col-[1/8]">
+                <div className="shadow-sm col-[6_/_-1] absolute top-1 right-0 max-w-[250px] row-span-full -z-10 w-full 
+                group-odd:col-[1/8] group-odd:left-0 sm:max-w-full sm:col-[6_/_-1] sm:relative">
                   <a href={external || github || '#'}>
                     <div className="relative overflow-hidden">
                       <div className="w-full" />
-                      <Image 
-                        src={cover} 
-                        alt={title} 
+                      <Image
+                        src={cover}
+                        alt={title}
                         width={520}
                         height={324}
-                        className="object-cover object-[center_center] max-w-full rounded-xl top-0 left-0 w-full h-full" 
+                        className="object-cover object-[center_center] max-w-full rounded-xl top-0 left-0 w-full h-full 
+                          max-[400px]:brightness-75 brightness-90"
                       />
                     </div>
                   </a>
