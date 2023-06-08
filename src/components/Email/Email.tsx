729f544b3952701a4dee7d9d94bb7bfcc989cf75
Alberto Rodriguez <ticoc911@gmail.com>
2023-06-08 09:35:18 +0200

    email added

--- src/components/Email/Email.tsx
+++ src/components/Email/Email.tsx
@@ -1,7 +1,17 @@
+import { email } from "@/data"
 
 
 export const Email = () => {
   return (
-    <div>Email</div>
+    <div className='w-10 fixed bottom-0 right-8 z-30 text-white hidden lg:block'>
+      <div className="flex items-center relative">
+        <a 
+          href={`mailto:${email}`} 
+          className="[writing-mode:vertical-rl] my-5 p-[10px] font-space text-sm tracking-widest
+            transition hover:text-primary hover:translate-y-[-3px]">
+          {email}
+        </a>
+      </div>
+    </div>
   )
 }
