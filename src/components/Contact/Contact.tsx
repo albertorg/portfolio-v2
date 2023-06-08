55c181d40ad1094f9fabe0bdb6d7999fb8489b72
Alberto Rodriguez <ticoc911@gmail.com>
2023-06-08 06:06:09 +0200

    section contact done and h2 component updated

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,7 +1,24 @@
+import { Button, NumberedHeading } from "@/components"
 
 
 export const Contact = () => {
   return (
-    <div>Contact</div>
+    <section className="max-w-[600px] text-center">
+      <NumberedHeading title="What’s Next?" overline/>
+
+      <h2 className="text-[clamp(36px,_5vw,_40px)] font-medium text-white leading-9 mb-3">
+        Get In Touch
+      </h2>
+
+      <p>
+        Although I&apos;m not currently looking for any new opportunities, my inbox is always open.
+        Whether you have a question or just want to say hi, I&apos;ll try my best to get back to
+        you!
+      </p>
+
+      <div className="mt-10">
+        <Button text="Say Hello" />
+      </div>
+    </section>
   )
 }
--- src/components/NumberedHeading/NumberedHeading.tsx
+++ src/components/NumberedHeading/NumberedHeading.tsx
@@ -2,15 +2,16 @@ import { FC } from "react";
 
 interface Props {
     title: string;
+    overline: boolean;
 }
 
-export const NumberedHeading: FC<Props> = ({title}) => {
+export const NumberedHeading: FC<Props> = ({ title, overline = false }) => {
     return (
-        <h2 className='flex items-center relative mt-2 mr-0 mb-10 w-full font-medium text-dynamicH2 text-white whitespace-nowrap
-        before:relative before:bottom-0 before:[counter-increment:section] before:content-["0"_counter(section)_"."]  
-        before:text-primary before:font-space before:text-dynamicSection before:mr-1 before:mb-[-5px] md:before:mr-4
-        after:block after:relative after:top-0 after:w-full after:h-[1px] after:ml-3 after:bg-primary sm:after:ml-5 
-        sm:after:w-[200px] md:after:w-[300px]'
+        <h2
+            className={`flex items-center ${overline && 'justify-center mb-8 text-primary'} relative mt-2 mr-0 mb-10 w-full font-medium ${!overline && ''} whitespace-nowrap
+            before:relative before:bottom-0 before:[counter-increment:section] before:content-["0"_counter(section)_"."]  
+            before:text-primary before:font-space before:mr-1 md:before:mr-4
+            ${!overline && 'after:block after:relative after:top-0 after:w-full after:h-[1px] after:ml-3 after:bg-primary sm:after:ml-5 sm: after: w-[200px]md: after: w - [300px] before:text-dynamicSection before:mb-[-5px] text-dynamicH2 text-white'}`}
         >
             {title}
         </h2>
