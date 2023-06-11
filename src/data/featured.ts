63981caa039a4ffcee9c113547fe29f6f2cddfbe
Alberto Rodriguez <ticoc911@gmail.com>
2023-06-12 00:36:41 +0200

    featured info updated

--- src/data/featured.ts
+++ src/data/featured.ts
@@ -2,28 +2,28 @@ export const featured = [
   {
     title: 'Fun Travels',
     cover: '/fun-travels.webp',
-    github: 'https://github.com/nextui-org/nextui',
-    external: 'https://nextui.org',
+    github: 'https://github.com/albertorg/Travel-Agency',
+    external: 'https://funtravels.net/',
     descriptionHtml:
-      'React UI library with SSR support, fully customizable, responsive adaptative components, dark mode support, beautiful, modern and fast. <a target="_blank" rel="noreferrer" href="https://nextui.org">Go there</a> ',
+      'This website, developed from scratch, features a real hotel booking <a target="_blank" rel="noreferrer" href="https://nextui.org">API from Hotelbeds</a>. Although it is a work in progress, the main functionalities are fully functional, showcasing the capabilities of the platform. ',
     techs: ['React', 'Javascript', 'Express']
   },
   {
     title: 'RESDM',
     cover: '/resdm.webp',
-    github: 'https://github.com/jrgarciadev/dev-cover',
-    external: 'https://devcover.me',
+    external: 'https://resdm.com/',
     descriptionHtml:
-      'I won the <a target="_blank" rel="noreferrer" href="https://vercel.com">Vercel</a> & <a target="_blank" rel="noreferrer" href="https://hashnode.com">Hashnode</a> <a target="_blank" rel="noreferrer" href="https://townhall.hashnode.com/vercelhashnode-hackathon-winners">Hackaton</a> with this project which is the easiest way to generate a developer portfolio. Devcover collects the developer data from Github, Hashnode and Dev.to sites to build a great porfolio just with their Github username <a target="_blank" rel="noreferrer" href="https://blog.jrgarciadev.com/devcover-easiest-way-to-generate-a-developer-portfolio">Read blog</a> ',
+      'I participated in the front-end development of the RESDM project, a platform aimed at analyzing and optimizing wind and solar farms worldwide.  ',
     techs: ['Vue.js', 'Javascript', 'Laravel']
   },
   {
-    title: 'React Iconly',
+    title: 'Tejeda Ing.Büro',
     cover: '/tejeda.webp',
-    github: 'https://github.com/jrgarciadev/react-iconly',
-    external: 'https://react-iconly.jrgarciadev.com',
+    github: 'https://github.com/albertorg/tejeda-ing-buro',
+    external: 'https://www.tejedaingburo.com/',
     descriptionHtml:
-      'Beautiful and pixel perfect React Icon Library, <a target="_blank" rel="noreferrer" href="https://piqodesign.gumroad.com/l/iconly">Iconly</a> is one of the options that is being used by designers and developers today, so I decided to create a library for React / Next.js / Gatsby that facilitates its use and that also allows us to customize any icon according to our needs.  <a target="_blank" rel="noreferrer" href="https://dev.to/jrgarciadev/beautiful-and-pixel-perfect-react-icon-library-2fnd">Blog Post</a>',
+      'Website for a German company specialized in engineering and construction services. The development of the website was based on a design provided by the client, ensuring its correct implementation and functionality.',
     techs: ['React', 'Javascript', 'CSS']
   }
 ]
+
