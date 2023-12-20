501ec5d5c18f6f285221a0eb180fe88e50385f80
Alberto Rodriguez <ticoc911@gmail.com>
2023-12-20 15:26:32 +0100

    Add fantasiai

--- src/data/featured.ts
+++ src/data/featured.ts
@@ -1,29 +1,37 @@
 export const featured = [
   {
-    title: 'Fun Travels',
-    cover: '/fun-travels.webp',
-    github: 'https://github.com/albertorg/Travel-Agency',
-    external: 'https://funtravels.net/',
+    title: "FantasiAI",
+    cover: "/fantasi-ai.webp",
+    external: "https://fantasiai.com/",
+    descriptionHtml:
+      "Website capable of generating stories with beautiful illustrations through the OpenAI api, inspired by Tailwind styles. ",
+    techs: ["Nuxt", "TypeScript", "Tailwind"],
+  },
+  {
+    title: "Fun Travels",
+    cover: "/fun-travels.webp",
+    github: "https://github.com/albertorg/Travel-Agency",
+    external: "https://funtravels.net/",
     descriptionHtml:
       'This website, developed from scratch, features a real hotel booking <a target="_blank" rel="noreferrer" href="https://nextui.org">API from Hotelbeds</a>. Although it is a work in progress, the main functionalities are fully functional, showcasing the capabilities of the platform. ',
-    techs: ['React', 'Javascript', 'Express']
+    techs: ["React", "Javascript", "Express"],
   },
   {
-    title: 'RESDM',
-    cover: '/resdm.webp',
-    external: 'https://resdm.com/',
+    title: "RESDM",
+    cover: "/resdm.webp",
+    external: "https://resdm.com/",
     descriptionHtml:
-      'I participated in the front-end development of the RESDM project, a platform aimed at analyzing and optimizing wind and solar farms worldwide.  ',
-    techs: ['Vue.js', 'Javascript', 'Laravel']
+      "I participated in the front-end development of the RESDM project, a platform aimed at analyzing and optimizing wind and solar farms worldwide.  ",
+    techs: ["Vue.js", "Javascript", "Laravel"],
   },
-  {
-    title: 'Tejeda Ing.Büro',
-    cover: '/tejeda.webp',
-    github: 'https://github.com/albertorg/tejeda-ing-buro',
-    external: 'https://www.tejedaingburo.com/',
-    descriptionHtml:
-      'Website for a German company specialized in engineering and construction services. The development of the website was based on a design provided by the client, ensuring its correct implementation and functionality.',
-    techs: ['React', 'Javascript', 'CSS']
-  }
-]
+  // {
+  //   title: "Tejeda Ing.Büro",
+  //   cover: "/tejeda.webp",
+  //   github: "https://github.com/albertorg/tejeda-ing-buro",
+  //   external: "https://www.tejedaingburo.com/",
+  //   descriptionHtml:
+  //     "Website for a German company specialized in engineering and construction services. The development of the website was based on a design provided by the client, ensuring its correct implementation and functionality.",
+  //   techs: ["React", "Javascript", "CSS"],
+  // },
+];
 
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,26 +1,35 @@
 export const projects = [
   {
-    title: 'MERN Calendar',
-    external: 'https://calendar.albertorodriguez.link/',
-    github: 'https://github.com/albertorg/calendar-app',
+    title: "Tejeda Ing.Büro",
+    github: "https://github.com/albertorg/tejeda-ing-buro",
+    external: "https://www.tejedaingburo.com/",
     descriptionHtml:
-      'Full-stack development of a collaborative calendar with the ability to add, edit and delete events.',
-    techs: ['React.js', 'Node', 'MongoDB', 'Express']
+      "Website for a German company specialized in engineering and construction services.",
+    techs: ["React", "Javascript", "CSS"],
   },
   {
-    title: 'Portfolio',
-    external: 'https://albertorodriguez.link/',
-    github: 'https://github.com/albertorg/portfolio-v2',
+    title: "MERN Calendar",
+    external: "https://calendar.albertorodriguez.link/",
+    github: "https://github.com/albertorg/calendar-app",
+    descriptionHtml:
+      "Full-stack development of a collaborative calendar with the ability to add, edit and delete events.",
+    techs: ["React.js", "Node", "MongoDB", "Express"],
+  },
+  {
+    title: "Portfolio",
+    external: "https://albertorodriguez.link/",
+    github: "https://github.com/albertorg/portfolio-v2",
     descriptionHtml:
       "Personal portfolio as a web developer. The main objective is to show my skills, experience and projects in a professional and attractive design.",
-    techs: ['Next.js', 'Typescript', 'Tailwind']
+    techs: ["Next.js", "Typescript", "Tailwind"],
   },
   {
-    title: 'Journal App',
-    external: 'https://journal.albertorodriguez.link/',
-    github: 'https://github.com/albertorg/journal-app',
-    descriptionHtml: 'Journal App allows users to write and organize their thoughts, feelings and experiences digitally.',
-    techs: ['React.js', 'Javascript', 'Firebase, Redux']
+    title: "Journal App",
+    external: "https://journal.albertorodriguez.link/",
+    github: "https://github.com/albertorg/journal-app",
+    descriptionHtml:
+      "Journal App allows users to write and organize their thoughts, feelings and experiences digitally.",
+    techs: ["React.js", "Javascript", "Firebase, Redux"],
   },
   // {
   //   title: 'Framework components for Vue.js',
@@ -29,4 +38,4 @@ export const projects = [
   //   descriptionHtml: 'I sometimes contributed to a Vue.js components framework, Vuesax.',
   //   techs: ['Vue.js', 'Javascript', 'LESS']
   // }
-]
+];
